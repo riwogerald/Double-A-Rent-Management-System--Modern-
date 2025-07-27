9b4d2b66af1098e5c76f5b9eb45bda55a05544f6
riwogerald <riwo.gerald@outlook.com>
2025-07-27 08:52:12 +0300

    Update PropertyForm.tsx

--- src/components/forms/PropertyForm.tsx
+++ src/components/forms/PropertyForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { propertiesAPI, landlordsAPI, agentsAPI, estatesAPI } from '../../lib/api'
+import { landlordsAPI, agentsAPI, estatesAPI } from '../../lib/api'
 
 interface PropertyFormProps {
   property?: any
